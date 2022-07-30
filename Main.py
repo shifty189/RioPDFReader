f4971e9a14ab109875b2005f175f05f5b77d7689
shifty189 <shifty189@gmail.com>
2022-07-30 15:44:05 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,27 @@ co-worker of mine this is very much a work in progress. this repo is intended fo
 """
 
 from gtts import gTTS
-# from io import BytesIO
+from io import BytesIO
 from PyPDF2 import PdfReader
+from playsound import playsound
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
+
+
+def readPage(text):
+    print(text)
+    tts = gTTS(text=text)
+    tts.save('test.mp3')
+    # mp3_fp = BytesIO()
+    # tts.write_to_fp(mp3_fp)
+    playsound('test.mp3')
+
+
+def firstPage():
+    global dropVar, pageVar
+    print(pageVar.get())
+    dropVar.set(0)
+    rebuildFrame()
 
 
 def lastPage():
@@ -30,6 +47,16 @@ def pageUP():
     rebuildFrame()
 
 
+def pageDOWN():
+    global dropVar, pageVar
+    if dropVar.get() > 0 :
+        test = dropVar.get() - 1
+    else:
+        test = dropVar.get()
+    dropVar.set(test)
+    rebuildFrame()
+
+
 def makeList():
     global pageVar
     size = pageVar.get()
@@ -42,7 +69,7 @@ def makeList():
     return temp
 
 def drawFrame(*args):
-    global FilePath, Frame, pageVar, dropVar
+    global FilePath, Frame, pageVar, dropVar, readButton
     # print('draw frame is running')
     PDF = PdfReader(FilePath, False)
     Doc_Length = len(PDF.pages)
@@ -51,14 +78,23 @@ def drawFrame(*args):
     text = page.extract_text()
 
     dropDownList = makeList()
+
+    readButton = tk.Button(Frame, text="Read Page", command=lambda: readPage(text))
+    readButton.grid(row=0, column=2)
+
+    firstPageButton = tk.Button(Frame, text="<<", command=firstPage)
+    firstPageButton.grid(row=1, column=0)
+    prevPageButton = tk.Button(Frame, text="<", command=pageDOWN)
+    prevPageButton.grid(row=1, column=1)
     pageDropdown = tk.OptionMenu(Frame, dropVar, *dropDownList, command=drawFrame)
-    displayLabel = tk.Label(Frame, text=text)
-    displayLabel.grid(row=1, column=0)
-    pageDropdown.grid(row=0, column=0)
+    pageDropdown.grid(row=1, column=2)
     nextPageButton = tk.Button(Frame, text=">", command=pageUP)
-    nextPageButton.grid(row=0, column=1)
+    nextPageButton.grid(row=1, column=3)
     lastPageButton = tk.Button(Frame, text=">>", command=lastPage)
-    lastPageButton.grid(row=0, column=2)
+    lastPageButton.grid(row=1, column=4)
+
+    displayLabel = tk.Label(Frame, text=text)
+    displayLabel.grid(row=2, column=0)
     print(text)
 
 
@@ -88,6 +124,7 @@ Frame.pack()
 #start up Tkinter objects
 open_button = tk.Button(Frame, text=f'Open File', command=openFile)
 open_button.grid(row=0, column=0)
+
 pageVar = tk.IntVar()
 dropVar = tk.IntVar()
 
